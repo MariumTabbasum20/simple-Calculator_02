c32431f791433f6a16da5137a15bb34c5931c1b3
MariumTabbasum20 <160420840+MariumTabbasum20@users.noreply.github.com>
2024-07-13 19:18:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    { message: "Enter first Number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// conditional statements if-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
